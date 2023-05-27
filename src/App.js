cbd1efd9a66f4e8b7c76c62c2808e291e2483cee
Brandt Cox <brandtleydavis@gmail.com>
2023-05-27 11:59:30 -0600

    deleting some index.css, component js changes to come

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import BucketList from './components/BucketList';
+import BucketList from './components/BucketList'; // Change this
+import React from 'react';
 
 function App() {
   return (
