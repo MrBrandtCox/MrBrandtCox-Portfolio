e3f6b1fd0c414f8c5aa9a8569f41c44b8455278b
Brandt Cox <brandtleydavis@gmail.com>
2023-05-30 23:23:46 -0600

    minor changes to footer css, email and phone will go to About page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,19 +9,13 @@ function Footer() {
             {/* <Link>to= '/' className='LinkedIn'</Link>
             <Link to= '/https://www.linkedin.com/in/brandt-cox-89bbbb254' className='LinkedIn'>LinkedIn</Link> */}
 
-            {/* Create Links for the contacts */}
+            {/* Create Links for the contacts, Email and Phone will be added to About page*/}
             <ul>
                 <li>
                     LinkedIn
                 </li>
                 <li>
                     Github
-                </li>
-                <li>
-                    Email
-                </li>
-                <li>
-                    Phone
                 </li>
                 <li>
                     Twitter
