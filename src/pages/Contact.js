764121eeac06b61157bae785d6af15f164de41dd
Brandt Cox <brandtleydavis@gmail.com>
2023-08-10 23:38:55 -0600

    updating to new email

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,7 +7,7 @@ function Contact () {
             <h2 className='contact-title'>Contact Me</h2>
             <section>
                 <ul className='contact-container'>
-                    <li className = 'email'><a target='_blank' rel='noreferrer noopener' href='https://mail.google.com/mail/u/0/#inbox?compose=new'>brandtleydavis@gmail.com</a></li>
+                    <li className = 'email'><a target='_blank' rel='noreferrer noopener' href='https://mail.google.com/mail/u/0/#inbox?compose=new'>brandtleydev@gmail.com</a></li>
                     <li className = 'linkedin'><a target='_blank' rel='noreferrer noopener' href='https://www.linkedin.com/in/brandt-cox-89bbbb254/'>LinkedIn</a></li>
                     <li className = 'phone'><p>Cell: (801)949-6230</p></li>
                 </ul>
